5fea49d6765212012987cfe758fd69398cc27421
justinbourb <30423126+justinbourb@users.noreply.github.com>
2020-10-15 13:36:08 -0400

    Update Cat.java

--- animal/Cat.java
+++ animal/Cat.java
@@ -4,21 +4,19 @@ import Animal.Animal;
 public class Cat extends Animal{
     private String sound;
     
-    /* comment out for now
+    
     public Cat (String breed, int lifespan, String behavior, double price){
-        this.breed = breed;
-        this.lifespan = lifespan;
-        this.behavior = behavior;
-        this.price = price;
+        //calls the super class constructor
+        super(breed, lifespan, behaviour, price);
     }
 
     public Cat(){
-        this.breed = null;
-        this.lifespan = 0;
-        this.behavior = null;
-        this.price = null;
+        //how to use blank constructors with super class constructor?
+        //just write super()!
+        super();
+        this.sound = null;
     }
-    */
+    
 
     public String getSound() {
         return "Meow!";

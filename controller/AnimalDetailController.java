d52681c951e777feecb15f92b86cff44775ef1ec
justinbourb <30423126+justinbourb@users.noreply.github.com>
2020-10-16 15:51:12 -0400

    Update AnimalDetailController.java

--- controller/AnimalDetailController.java
+++ controller/AnimalDetailController.java
@@ -1,4 +1,23 @@
 package controller;
 
-public class AnimalDetailController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class AnimalDetailController implements Initializable {
+    
+//TODO: add buttons,fields, etc from SceneBuilder
+    
+     @FXML
+    void onActionDisplayMainMenu(ActionEvent event) {
+        
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+
+
 }

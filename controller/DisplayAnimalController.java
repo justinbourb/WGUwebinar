5e66b4977f8e13cd0ca75ecd7b6eae28ebf6cba4
justinbourb <justinbourb@gmail.com>
2020-10-23 15:49:47 -0400

    search working for int or string

--- controller/DisplayAnimalController.java
+++ controller/DisplayAnimalController.java
@@ -8,12 +8,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -61,17 +63,10 @@ public class DisplayAnimalController implements Initializable {
     private Button backButton;
 
     @FXML
-    void onActionDisplayAnimalDetailsMenu(ActionEvent event) throws IOException {
-        String resourceURL = "/view/AnimalDetailMenu.fxml";
-        switchStage.switchStage(event, resourceURL);
-    }
-    
-     @FXML
-    void onActionDisplayMainMenu(ActionEvent event) throws IOException {
-         String resourceURL = "/view/MainMenu.fxml";
-         switchStage.switchStage(event, resourceURL);
-    }
+    private HBox topHBox;
 
+    @FXML
+    private TextField searchTextField;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -85,4 +80,50 @@ public class DisplayAnimalController implements Initializable {
         specialColumn.setCellValueFactory(new PropertyValueFactory<>("special"));
     }
 
+    @FXML
+    void onActionDisplayAnimalDetailsMenu(ActionEvent event) throws IOException {
+        String resourceURL = "/view/AnimalDetailMenu.fxml";
+        switchStage.switchStage(event, resourceURL);
+    }
+    
+     @FXML
+    void onActionDisplayMainMenu(ActionEvent event) throws IOException {
+         String resourceURL = "/view/MainMenu.fxml";
+         switchStage.switchStage(event, resourceURL);
+    }
+    @FXML
+    void onKeyReleasedSearchText(javafx.scene.input.KeyEvent keyEvent) {
+        //TODO: search() is working correctly, need to then filter results on screen.
+        //This should be covered in a future webinar
+        System.out.println(searchTextField.getText() + " entered into search");
+        System.out.println("Search results: " + search(searchTextField.getText()));
+    }
+
+    public boolean search(String searchInput) {
+        boolean itsInt;
+        int intSearch = -1;
+        String stringSearch = null;
+        //check if the search input is an int or a string
+        try {
+            intSearch = Integer.parseInt(searchInput, 10);
+            itsInt = true;
+        } catch (Exception e){
+            stringSearch = searchInput;
+            itsInt = false;
+        }
+        //if int, search against ID else search against Breed
+        for (Animal dog : DataProvider.getAllAnimals()){
+            if (itsInt){
+                if(dog.getId() == intSearch){
+                    return true;
+                }
+            } else {
+                if(dog.getBreed().contains(stringSearch)){
+                    return true;
+                }
+            }
+        }
+        //no match, return false
+        return false;
+    }
 }
--- controller/mainMenuController.java
+++ controller/mainMenuController.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
